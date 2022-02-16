e19381cabc4eab71c90b0d316de3dde4b9a7e742
Claudio F <claudio@monogram.io>
2022-02-16 09:55:36 -0500

    added commander and a few actions to run

--- mes.mjs
+++ mes.mjs
@@ -1,16 +1,35 @@
 #! /usr/bin/env node
 
-import { $, argv, cd, chalk, fs, question, fetch } from 'zx'
+import { Command } from 'commander'
+import { $, argv, cd, chalk, fs, fetch } from 'zx'
 $.verbose = false
 
+let packageJson = await fs.readFile('./package.json')
+packageJson = JSON.parse(packageJson)
+
+// Commander
+const program = new Command()
+program
+	.name('string-util')
+	.description('Monogram Env Sync (`mes`) CLI')
+	.version(packageJson.version)
+program
+	.option(
+		'-i, --init <orgId> <apikey>',
+		'Initialize a new environment file as ".env.local" using the provided org ID and API key'
+	)
+	.option('-ef, --env-file <filename>', 'Path to .env file', '.env.local')
+program.parse()
+
+// START HERE
 const envFile = await readEnvFile()
 const envFileLines = envFile.split('\n')
 
 // Get the API Key
-const menvApiKey = envFileLines.find((line) => line.startsWith('MNV_API_KEY')).split('=')[1]
+const menvApiKey = envFileLines.find((line) => line.startsWith('MES_API_KEY')).split('=')[1]
 
 async function readEnvFile() {
-	const envFileName = argv.env || '.env.local'
+	const envFileName = options.envFile || '.env.local'
 	return await (
 		await $`cat ${envFileName}`
 	).stdout

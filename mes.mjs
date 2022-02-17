2e9e3e8618fb6ac094d77918861f525164395dd3
Claudio F <claudio@monogram.io>
2022-02-17 11:43:31 -0500

    split up the different commands

--- mes.mjs
+++ mes.mjs
@@ -10,30 +10,43 @@ packageJson = JSON.parse(packageJson)
 // Commander
 const program = new Command()
 program.name('mes').description('Monogram Env Sync (`mes`) CLI').version(packageJson.version)
+
 program
-	.option(
-		'-i, --init <orgId> <apikey>',
-		'Initialize a new environment file as ".env.local" using the provided org ID and API key'
-	)
+	.command('init')
+	.description('Initialize a new project')
+	.argument('<orgId>', 'Organization ID')
+	.argument('<apikey>', "Organization's API Key")
+	.action(async (orgId, apikey) => {
+		console.log(orgId, apikey)
+	})
+
+program
+	.command('sync')
+	.description('Sync the local environment file with the remote environment file.')
 	.option('-e, --env-file <filename>', 'Path to .env file', '.env.local')
 	.option('-s, --sync', 'Sync the .env file to the environment with the server')
+	.action(async () => {
+		// Parse options
+		const options = program.opts()
+		const envFileName = options.envFile || '.env.local'
+
+		// Read the local environment file
+		const envFile = await readEnvFile(envFileName)
+		const envFileLines = envFile.split('\n')
+
+		// Get the API Key
+		const menvApiKey = envFileLines.find((line) => line.startsWith('MES_API_KEY')).split('=')[1]
+
+		console.log(menvApiKey)
+	})
+
 program.parse()
 
-// START HERE
-const envFile = await readEnvFile()
-const envFileLines = envFile.split('\n')
-
-// Get the API Key
-const menvApiKey = envFileLines.find((line) => line.startsWith('MES_API_KEY')).split('=')[1]
-
-async function readEnvFile() {
-	const envFileName = options.envFile || '.env.local'
+async function readEnvFile(envFileName) {
 	return await (
 		await $`cat ${envFileName}`
 	).stdout
 }
-
-console.log(menvApiKey)
 
 async function getProjectDetails() {
 	let resp = await fetch(`https://api.menv.com/v1/projects/${argv.projectId}`, {
@@ -42,8 +55,3 @@ async function getProjectDetails() {
 		}
 	})
 }
-
-// let resp = await fetch('https://api.chucknorris.io/jokes/random')
-// if (resp.ok) {
-// 	console.log((await resp.json()).value)
-// }
